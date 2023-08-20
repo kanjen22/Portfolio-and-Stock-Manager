531dab19630c7653d06fbded7cb72d202a6dc605
KanJen-Liu <62913252+KanJen-Liu@users.noreply.github.com>
2023-08-19 21:51:36 -0700

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+'''
+Shift + Enter to open python debig console
+'''
 import os
-
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
