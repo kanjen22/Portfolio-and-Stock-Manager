286232b34a6a868f8c98bdb5296058b4e1ded98f
Kan-Jen Liu <kanjenuw@gmail.com>
2024-06-13 12:27:13 -0700

    update app.py from CS50 course web

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-'''
-Shift + Enter to open python debig console
-'''
 import os
+
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
